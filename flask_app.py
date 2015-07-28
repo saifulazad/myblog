8be37019cf1968d4c91148779814b319cbb11787
saifulazad <sai108710@gmail.com>
2015-07-28 23:08:08 +0600

    add

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import render_template, session,request ,redirect
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return render_template('index.html', user='Azad')
+
+    return 'Hello from Flask!'	
+
+
+app.run(host = '0.0.0.0',debug=True)
---
+++ run.py
@@ -0,0 +1,11 @@
+#!flask/bin/python
+from app import app
+
+
+
+
+
+app.run(host = '0.0.0.0',debug=True)
+
+#hi all
+# I have add a new module
