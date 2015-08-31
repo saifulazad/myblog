55afbdbf893a8ac9b52efbd28d91f72cc822075d
saifulazad <sai108710@gmail.com>
2015-08-31 22:41:59 +0600

    dgdfgdfg

--- flask_app.py
+++ flask_app.py
@@ -6,9 +6,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('base.html', user='Azad')
+    return render_template('home.html', user='Azad')
 
     return 'Hello from Flask!'	
+
+@app.route('/python')
+def hello():
+    return render_template('topic.html', chapters = [("ggg", "as"),("Reference Books", "ref_books")])
+@app.route('/ref_books')
+def ref_books():
+
+    books = [
+
+        {
+            'Author': 'Azad',
+            'Name': 'Learning Python',
+            'Publication': 'Orelly',
+            'Descriptions':'This is very nice and cool book ever I have seen',
+             'imgURL': 'learing_py.jpeg',
+            'AmazonURL': 'http://www.amazon.com/Learning-Python-Edition-Mark-Lutz/dp/1449355730',
+        },
+        {
+            'Author': 'Azad',
+            'Name': 'Dive into Python',
+            'Publication': 'Orelly',
+            'Descriptions':'This is very bad and cool book ever I have seen',
+            'imgURL': 'dive_into_python.jpg',
+            'AmazonURL': 'http://www.amazon.com/Dive-Into-Python-Mark-Pilgrim/dp/1590593561',
+        }
+
+    ]
+
+
+    return render_template('ref_books.html', books = books ,chapters = [("ggg", "as"),("Reference Books", "ref_books")])
+
 
 
 
